e02401ec48fd17c655dab26bae465db8879ea22b
AlinaKa7 <7k.alina32@gmail.com>
2023-04-27 12:06:38 +0300

    correct variant

--- index.js
+++ index.js
@@ -8,3 +8,18 @@ alert('If to add, the result is ' + adding);
 alert('If to substract, the result is ' + minus);
 alert('If to multiply, the result is ' + multiply);
 alert('If to devide, the result is ' + devide);
+
+//правильний варіант:
+
+first = +prompt('Please enter first value.'); //одразу додати + щоб перетворити строку у цифру, бо prompt перетворює у строку
+second = +prompt('Please enter second value.');
+adding = first + second;
+minus = first - second;
+multiply = first * second;
+devide = first / second;
+alert(`Your result:
+    adding: ${adding};
+    minus: ${minus};
+    multiply: ${multiply};
+    devide: ${devide};
+`);
